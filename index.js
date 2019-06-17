52c96b49380b2e37911a3605bc3af9a682ef3cb3
adv <helloworld@fakeemail>
2019-06-17 21:15:11 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       <your-email>
+// @match        https://*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @grant       GM_xmlhttpRequest
+// @noframes
+// @connect localhost
+
+// ==/UserScript==
+
+/* global $ */
+
+(function () {
+  'use strict'
+
+})()
---
+++ readme.md
@@ -0,0 +1,15 @@
+# USERSCRIPT BOILERPLATE
+
+## INGRIDIENT
+- eslint
+- eslint-standard
+- terserjs for minify the code
+- editorconfig
+
+## INSTALL
+- `yarn install`
+- Change `index.js`
+
+## SCRIPT
+- `yarn run lint` - Lint the code
+- `yarn run build` - Lint and build the code. Find it in `dist` folder
